243b98807381d78fab1b95122d3ae144c2cabfb0
jayce0902 <132318529+jayce0902@users.noreply.github.com>
2023-06-19 16:07:21 +0800

    Update streamlit_app.py
    to update fruit advice from fruitvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #removed line as it outputs raw JSON
 
 # take json version of response and NORMALIZE IT
