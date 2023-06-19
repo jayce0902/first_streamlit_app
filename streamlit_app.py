400c3d0bdf74c9d689e7d46c65b039b31c905c9f
jayce0902 <132318529+jayce0902@users.noreply.github.com>
2023-06-19 16:03:48 +0800

    Update streamlit_app.py
    Normalize the advice section w/ JSON to dataframe and delete the JSON output

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,10 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) #removed line as it outputs raw JSON
+
+# take json version of response and NORMALIZE IT
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the output as dataframe
+streamlit.dataframe(fruityvice_normalized)
